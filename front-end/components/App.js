f73f66f2bfe8f19a1789e3fbf19a87ed7562c9a3
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-05-11 21:43:04 -0500

    Created the bones of the file structure.

---
+++ front-end/components/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { MainContext } from '../context.js';
+import Axios from 'axios';
+
+const App = () => {
+
+  return (
+    <div>Hello World!</div>
+  )
+}
