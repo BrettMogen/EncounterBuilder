96b8d62afff391ed10c1bfff314b54072d1a2197
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-08-28 14:37:25 -0500

    Saved changes from last session.

--- front-end/components/EnemyTeam.js
+++ front-end/components/EnemyTeam.js
@@ -3,7 +3,7 @@ import React, { useState, useContext } from 'react';
 const EnemyTeam = () => {
   return (
     <div>
-      <div className="enemyTeam"></div>
+      <div className="enemyTeamContainer"></div>
    </div>
   )
 }
--- front-end/components/FriendlyParty.js
+++ front-end/components/FriendlyParty.js
@@ -1,9 +1,13 @@
 import React, { useState, useContext } from 'react';
 
 const FriendlyParty = () => {
+  let friendlyParty = [<div>Hello</div>, <div>Hi there</div>, <div>Howdy</div>, <div>What happens now?</div>];
+
   return (
     <div>
-      <div className="friendlyParty"></div>
+      <div className="friendlyPartyContainer">
+        <div className="friendlyParty">{friendlyParty}</div>
+      </div>
     </div>
   )
 }
