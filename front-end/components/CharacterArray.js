89cd3db31440707395cccda89ed253c486a5aadf
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-10-03 18:11:49 -0500

    Made edits to the first two characters in the Charachters Array.

--- front-end/components/CharacterArray.js
+++ front-end/components/CharacterArray.js
@@ -1,3 +1,5 @@
+//Characters referenced from lvl. 5 custom-created characters on DnDBeyond
+
 const characters = [
   {
     id: '0',
@@ -8,40 +10,97 @@ const characters = [
     cssName: 'ranger',
     cssBorderColor: '#295e21',
     weapon: 'Bow',
-    armour: 14,
-    health: 12,
+    armour: 15,
+    health: 39,
     description: 'Far from the bustle of cities and towns, past the hedges that shelter the most distant farms from the terrors of the wild, amid the dense-packed trees of trackless forests and across wide and empty plains, rangers keep their unending watch.',
     image: './images/Ranger.png',
     baseStats: {
-      strength: 10,
-      dexterity: 15,
-      constitution: 12,
+      strength: 11,
+      dexterity: 18,
+      constitution: 13,
       intelligence: 9,
-      wisdom: 11,
-      charisma: 10
+      wisdom: 14,
+      charisma: 15
     },
     actions: [
       {
+        name: 'Longbow',
         type: 'attack',
-        name: 'Bow Attack',
-        hitChance: 5,
+        hitChance: 9,
         damage: {
-          numberOfDice: 2,
-          kindOfDice: 6,
-          baseDamage: 3
+          numberOfDice: 1,
+          kindOfDice: 8,
+          baseDamage: 4
         }
+      },
+      {
+        name: 'Dagger',
+        type: 'attack',
+        hitChance: 7,
+        damage: {
+          numberOfDice: 1,
+          kindOfDice: 4,
+          baseDamage: 4
+        }
+      },
+      {
+        name: 'Handaxe',
+        type: 'attack',
+        hitChance: 3,
+        damage: {
+          numberOfDice: 1,
+          kindOfDice: 6,
+          baseDamage: 0
+        }
+      },
+      {
+        name: 'Cure Wounds',
+        type: 'spell',
+        concentration: false,
+        healing: {
+          numberOfDice: 1,
+          kindOfDice: 8,
+          baseHealing: 2
+        }
+      }, 
+      {
+        //Choose a friendly target and change their AC to 16 if it is below 16
+        name: 'Bark Skin',
+        type: 'spell',
+        concentration: true,
+        newAC: '16'
       }
     ],
     bonusActions: [
       {
-        type: 'attack',
-        name: 'Dagger Attack',
-        hitChance: 2,
+        //Mark an enemy. The enemy takes an additional 1d6 damage from weapon attacks made by the character who marked it
+        name: 'Hunter\'s Mark',
+        type: 'spell',
+        concentration: true,
         damage: {
-          numberOfDice: 2,
-          kindOfDice: 4,
-          baseDamage: 2
+          numberOfDice: 1,
+          kindOfDice: 6
         }
+      }
+    ],
+    reactions: [
+      {
+        //If the incoming damage is acid, cold, fire, lightning, or thunder damage, absorb 1d6 of the damage and store it. 
+        //Until the start of your next turn, you have resistence to whatever damage type was stored. 
+        //Your first melee attack this turn does 1d6 extra damage for whatever type you stored
+        name: 'Absorb Elements',
+        type: 'spell',
+        concentration: false,
+        absorb: {
+          numberOfDice: 1,
+          kindOfDice: 6
+        }
+      }
+    ],
+    other: [
+      {
+        name: 'Extra Attack',
+        description: 'When an attack with a weapon is made, make an extra attack.'
       }
     ]
   },
@@ -54,40 +113,68 @@ const characters = [
     cssName: 'barbarian',
     cssBorderColor: '#9c2129',
     weapon: 'Great Axe',
-    armour: 16,
-    health: 20,
+    armour: 15,
+    health: 55,
     description: 'Barbarians, different as they might be, are defined by their rage: unbridled, unquenchable, and unthinking fury. More than a mere emotion, their anger is the ferocity of a cornered predator, the unrelenting assault of a storm, the churning turmoil of the sea. For some, their rage springs from a communion with fierce animal spirits. Others draw from a roiling reservoir of anger at a world full of pain. For every barbarian, rage is a power that fuels not just a battle frenzy but also uncanny reflexes, resilience, and feats of strength.',
     image: './images/Barbarian.png',
     baseStats: {
-      strength: 16,
-      dexterity: 10,
-      constitution: 14,
-      intelligence: 8,
-      wisdom: 10,
-      charisma: 11
+      strength: 17,
+      dexterity: 14,
+      constitution: 16,
+      intelligence: 9,
+      wisdom: 11,
+      charisma: 13
     },
     actions: [
       {
+        name: 'Greataxe',
         type: 'attack',
-        name: 'Axe Attack',
         hitChance: 6,
         damage: {
-          numberOfDice: 2,
+          numberOfDice: 1,
+          kindOfDice: 12,
+          baseDamage: 3
+        }
+      },
+      {
+        name: 'Greatclub',
+        type: 'attack',
+        hitChance: 6,
+        damage: {
+          numberOfDice: 1,
           kindOfDice: 8,
-          baseDamage: 4
+          baseDamage: 3
+        }
+      },
+      {
+        name: 'Javelin',
+        type: 'attack',
+        hitChance: 6,
+        damage: {
+          numberOfDice: 1,
+          kindOfDice: 6,
+          baseDamage: 3
         }
       }
     ],
     bonusActions: [
       {
-        type: 'attack',
-        name: 'Dagger Attack',
-        hitChance: 2,
-        damage: {
-          numberOfDice: 2,
-          kindOfDice: 4,
-          baseDamage: 2
-        }
+        name: 'Rage',
+        type: 'self-buff',
+        description: 'Choose to enter a rage. While raging, deal an extra +2 melee damage and gain resistance to physical attacks. The rage ends at the end of this character\'s turn if this character has not made an attack or taken damage since the start of its last turn.',
+      },
+      {
+        name: 'Frenzied Rage',
+        type: 'self-buff',
+        description: 'If already raging, this character can progress into a frenzied rage. While frenzied, this character can make an extra attack on it\'s bonus action so long as it\'s rage doesn\'t end.'
+      }
+    ],
+    reactions: [
+    ],
+    other: [
+      {
+        name: 'Extra Attack',
+        description: 'When an attack with a weapon is made, make an extra attack.'
       }
     ]
   },
