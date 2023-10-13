1bb405f8b0a3bd1f9d78e2cb19081b540716c0f8
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-10-12 23:41:58 -0500

    Added a fight button which will start combat sessions. No javascript has been included yet.

--- front-end/components/App.js
+++ front-end/components/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { MainContext } from '../context.js';
-import Axios from 'axios';
 import CharacterOptions from './CharacterOptions.js';
 import FriendlyParty from './FriendlyParty.js';
 import EnemyTeam from './EnemyTeam.js';
-import UniqueCharacters from './UniqueCharacters.js';
+import Fight from './Fight.js';
 
 const App = () => {
   const [characterOptionsArray, setCharacterOptionsArray] = useState([]);
@@ -30,6 +29,7 @@ const App = () => {
         </div>
         <div className="teamsContainer">
           <FriendlyParty />
+          <Fight />
           <EnemyTeam />
         </div>
       </MainContext.Provider>
---
+++ front-end/components/Fight.js
@@ -0,0 +1,12 @@
+import React, { useState, useContext, useEffect } from 'react';
+
+const Fight = () => {
+
+  return (
+    <div className="fightContainer">
+      <div className="fightButtonSmall">Fight!</div>
+    </div>
+  )
+}
+
+export default Fight;
