6214ee44b55b708df1e71c41b6f1d5bb42a169ef
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-11-28 15:59:59 -0600

    RollingInitiative.js now has the ability to loop through both teams at a times interval.

--- front-end/components/RollingInitiative.js
+++ front-end/components/RollingInitiative.js
@@ -1,31 +1,49 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import { MainContext } from '../context.js';
 
 const RollingInitiative = () => {
   const { friendlyParty, setFriendlyParty, enemyTeam, setEnemyTeam } = useContext(MainContext);
 
- const [topText, setTopText] = useState(0);
+
+  const [currentList, setCurrentList] = useState(friendlyParty);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [listIndex, setListIndex] = useState(0);
 
   useEffect(() => {
-    const updateCurrentCharacter = function() {
-      setTimeout(() => {
-        let newTopText = topText + 1;
-        setTopText(newTopText);
-        console.log('topText', topText);
-        updateCurrentCharacter();
-      }, 3000);
-    }
-   updateCurrentCharacter();
-  }, []);
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => {
+        const nextIndex = prevIndex + 1;
 
-  console.log('I rerendered!');
+        if ((currentList === enemyTeam) && (nextIndex > currentList.length)) {
+          clearInterval(intervalId); // Stop the interval
+          console.log('Both lists are exhausted. Do something else.');
+        } else if (prevIndex < currentList.length) {
+          // Display information from the current list
+          console.log('Displaying information for:', currentList[prevIndex].props.character.name);
+          if ((currentList === friendlyParty) && (nextIndex === currentList.length)) {
+            setCurrentList(enemyTeam);
+            setListIndex(listIndex + 1);
+          }
+        } 
+
+        if ((currentList === enemyTeam) && (nextIndex === currentList.length)) {
+          return nextIndex;
+        }
+        return nextIndex % currentList.length; // Reset to 0 at the end of the list
+      });
+    }, 3000);
+
+    // Cleanup the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [currentList, friendlyParty, enemyTeam, listIndex]);
+
   const generateD20 = function () {
     return Math.floor(20 * Math.random()) + 1;
   }
 
   return (
     <div className="rollingInitiativeContainer">
-      <div className="rollingInitiativeTopText">{topText.toString()}</div>
+      <div className="rollingInitiativeTopText"></div>
       <div className="rollingInitiativeDiceDisplay"></div>
       <div className="rollingInitiativeBottomText"></div>
     </div>
