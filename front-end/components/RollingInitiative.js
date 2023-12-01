5d40d1e159f511b60e2cbe4f6d948086a6e9be2d
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-12-01 17:10:19 -0600

    Major progress on the initiative rolling logic. All three pieces of the center display are updating mostly appropriately. The first index in friendlyParty is no longer rendering twice. CSS and timings need cleaning up, but after more logic can be inserted to further build out this section.

--- front-end/components/RollingInitiative.js
+++ front-end/components/RollingInitiative.js
@@ -1,74 +1,69 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { MainContext } from '../context.js';
 
 const RollingInitiative = () => {
-  const { friendlyParty, setFriendlyParty, enemyTeam, setEnemyTeam } = useContext(MainContext);
+  const { friendlyParty, enemyTeam } = useContext(MainContext);
 
   const [currentList, setCurrentList] = useState(friendlyParty);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [listIndex, setListIndex] = useState(0);
   const [topText, setTopText] = useState('');
   const [displayedDie, setDisplayedDie] = useState('');
   const [bottomText, setBottomText] = useState('');
-  
-  const startRolling = function(rollCounter){
-    const currentRoll = generateD20();
-    console.log('rollCounter', rollCounter);
-    rollCounter = rollCounter + 1;
-    setDisplayedDie(<div className={`D20-${currentRoll} enterDie`}></div>);
 
-    if (rollCounter < 5) {
+  const generateD20 = () => Math.floor(20 * Math.random()) + 1;
+
+  const startRolling = (index, rollCounter) => {
+    const currentRoll = generateD20();
+    console.log('Rolling for index', index);
+
+    setDisplayedDie(<div className={`D20-${currentRoll} enterDie`}></div>);
+    
+
+   
+
+    if (rollCounter < 3) {
+      const newRollCounter = rollCounter + 1;
+
       setTimeout(() => {
         setDisplayedDie(<div className={`D20-${currentRoll} exitDie`}></div>);
-      }, 500);
-      setTimeout(() => {
-        startRolling(rollCounter);
       }, 750);
-    } else if (rollCounter >= 5) {
-      const finalInitiative = currentRoll + currentList[currentIndex].props.character.initiative;
+     
       setTimeout(() => {
-        setBottomText(<div>{currentList[currentIndex].props.character.name} rolled a {currentRoll} + {currentList[currentIndex].props.character.initiative} for a total of {finalInitiative}</div>);
-      }, 500);
+        startRolling(index, newRollCounter);
+      }, 1000);
+    } else {
       setTimeout(() => {
-        setDisplayedDie(<div className={`D20-${currentRoll} exitDie`}></div>);
-      }, 3000);
+        const finalInitiative = currentRoll + currentList[index].props.character.initiative;
+        setBottomText(<div>{currentList[index].props.character.name} rolled a {currentRoll} + {currentList[index].props.character.initiative} for a total of {finalInitiative}</div>);
+      }, 2000);
     }
   };
 
   useEffect(() => {
+    setTopText(<div className="topTextEnters">{currentList[currentIndex].props.character.name} is rolling initiative...</div>);
+    setBottomText(<div></div>);
+    startRolling(currentIndex, 0);
+  }, [currentIndex]);
+
+  useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prevIndex) => {
-        const nextIndex = prevIndex + 1;
-
-        if (prevIndex < currentList.length) {
-          // Display information from the current list
-          setTopText(<div className="topTextEnters">{currentList[prevIndex].props.character.name} is rolling initiative...</div>);
-          setTimeout(() => {
-            startRolling(0);
-          }, 1000);
-          console.log('Displaying information for:', currentList[prevIndex].props.character.name);
+        if (prevIndex < currentList.length - 1) {
+          return prevIndex + 1;
         } else {
-          // Switch to the next list or perform a different action
-          if (currentList === friendlyParty && nextIndex === currentList.length) {
+          if (currentList === friendlyParty) {
             setCurrentList(enemyTeam);
-            setListIndex((prevListIndex) => prevListIndex + 1);
-          } else if (currentList === enemyTeam && nextIndex === currentList.length) {
-            clearInterval(intervalId); // Stop the interval
+          } else if (currentList === enemyTeam) {
+            clearInterval(intervalId);
             console.log('Both lists are exhausted. Do something else.');
           }
+          return 0;
         }
-
-        return nextIndex % currentList.length; // Reset to 0 at the end of the list
       });
     }, 12000);
 
-    // Cleanup the interval on component unmount
     return () => clearInterval(intervalId);
-  }, [currentList, friendlyParty, enemyTeam, listIndex]);
-
-  const generateD20 = () => {
-    return Math.floor(20 * Math.random()) + 1;
-  };
+  }, [currentList, friendlyParty, enemyTeam]);
 
   return (
     <div className="rollingInitiativeContainer">
