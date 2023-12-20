b7f1f03ac821fbcbfcc7a9e755d81c9b6065cc95
Brett Mogen <brett.mogen@jacks.sdstate.edu>
2023-12-20 14:20:59 -0600

    Updated fade-in and fade-out effects for text and dice in RollingInitiative.js

--- front-end/components/RollingInitiative.js
+++ front-end/components/RollingInitiative.js
@@ -8,9 +8,10 @@ const RollingInitiative = () => {
   const [displayedDie, setDisplayedDie] = useState(''); // reset to ''
   const [bottomText, setBottomText] = useState(''); //reset to ''
 
-
+  //helper function to generate new D20 rolls
   const generateD20 = () => Math.floor(20 * Math.random()) + 1;
-
+  
+  //control dice entering and leaving, then handle bottom text entering as well as all sections leaving(final stage for each character)
   const startRolling = (currentCharacter, rollCounter) => {
     const currentRoll = generateD20();
 
@@ -27,19 +28,43 @@ const RollingInitiative = () => {
         startRolling(currentCharacter, newRollCounter);
       }, 1000);
     } else {
+      const finalInitiative = currentRoll + currentCharacter.props.character.initiative;
+      const aOrAn = 'a';
+      if (currentRoll === 8 || currentRoll === 11 || currentRoll === 18) {
+        aOrAn = 'an';
+      }
+
+      //reveal the bottom text after the last dice roll has been made and calculate the final roll total
       setTimeout(() => {
-        const finalInitiative = currentRoll + currentCharacter.props.character.initiative;
-        setBottomText(<div>{currentCharacter.props.character.name} rolled a {currentRoll} + {currentCharacter.props.character.initiative} for a total of {finalInitiative}</div>);
+        setBottomText(<div className="bottomTextEnters">{currentCharacter.props.character.name} rolled {aOrAn} {currentRoll} + {currentCharacter.props.character.initiative} for a total of {finalInitiative}</div>);
       }, 2000);
+
+      //all three portions fade out in unison 
+      setTimeout(() => {
+        setTopText(<div className="topTextExits">{currentCharacter.props.character.name} is rolling initiative...</div>);
+        setDisplayedDie(<div className={`D20-${currentRoll} exitDieFinal`}></div>);
+        setBottomText(<div className="bottomTextExits">{currentCharacter.props.character.name} rolled {aOrAn} {currentRoll} + {currentCharacter.props.character.initiative} for a total of {finalInitiative}</div>);
+        //after all three portions fade out, set those states to be empty just to make sure the display is completely clean before the next character is displayed
+        setTimeout(() => {
+          setTopText(<div></div>);
+          setDisplayedDie(<div></div>);
+          setBottomText(<div></div>);
+        }, 1000);
+      }, 5000);
     }
   };
 
+  //top text enters, and startRolling is called to begin dice being displayed for that character
   const updateNextCharacter = function (currentCharacter) {
     setTopText(<div className="topTextEnters">{currentCharacter.props.character.name} is rolling initiative...</div>);
     setBottomText(<div></div>);
-    startRolling(currentCharacter, 0);
+    setTimeout(() => {
+      startRolling(currentCharacter, 0);
+    }, 1500);
+
   }
 
+  //runs once on initial render to begin cycling through each character and sending them to updateNextCharacter. After parties are exhausted, do something else
   useEffect(() => {
     let counter = 0;
     let currentParty = friendlyParty;
